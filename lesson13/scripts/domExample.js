008122eabee518dfdd8466605142d62907977721
Mikael Malania <m.malania@noxtton.com>
2024-11-09 18:15:28 +0400

    a

---
+++ lesson13/scripts/domExample.js
@@ -0,0 +1,6 @@
+const randomText = document.getElementById('randomText');
+
+randomText.innerText =
+    'The paragraph text has been updated dynamically by using JavaScript.';
+
+document.querySelector('body').style.backgroundColor = '#93a8c9';
---
+++ lesson13/scripts/firstScript.js
@@ -0,0 +1,9 @@
+console.log(typeof 'Hello, World!');
+console.log(typeof 123, typeof 123.123);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(Date.now());
+console.log(typeof Symbol('symbol'));
